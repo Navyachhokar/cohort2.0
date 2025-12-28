1ff67e406162b0d09352a037352d56c8268fb65c
Navyachhokar <chhokarnavya@gmail.com>
2025-12-28 19:09:37 +0530

    built a virtual piano using keyboard event and mouse events

--- js/dom/day7/script.js
+++ js/dom/day7/script.js
@@ -1,52 +1,27 @@
-var main = document.querySelector('#main');
-var cursor = document.querySelector('#cursor');
+const keys =document.querySelectorAll('.piano-key');
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        const note = key.dataset.note;
+        const audio = new Audio(`sounds/${note}.mp3`);
+        audio.play();
+        key.classList.add('active');
+        setTimeout(() => key.classList.remove('active'), 200);
+    });
+});
 
-main.addEventListener('mousemove', (elem)=> {
-    cursor.style.left = elem.x + 'px';
-    cursor.style.top = elem.y + 'px';
+document.addEventListener("keypress", (e) => {
+    const keyPressed = e.key.toLowerCase();
+    const key = document.querySelector(`.piano-key[data-key="${keyPressed}"]`);
+    console.log(key);
+    if(!key) return;
     
-})
-
-// different mouse event listeners
-// mouseover
-main.addEventListener('mouseover',function(val){
-    // console.log('mouseover event performed');
-    // console.log(val); //gives event details
-})
-// dblclick
-main.addEventListener('dblclick',function(val){
-    // console.log('dblclick event performed');
-    // console.log(val); //gives event details
-})
-// click
-main.addEventListener('click',function(val){
-    // console.log('click event performed');
-    // console.log(val); //gives event details
-})
-// mouseleave
-main.addEventListener('mouseleave',function(val){
-    // console.log('mouseleave event performed');
-    // console.log(val);//gives event details
-})
-// mouseenter
-main.addEventListener('mouseenter',function(val){
-    // console.log('mouseenter event performed');
-    // console.log(val); //gives event details
-})
-// mousemove
-main.addEventListener('mousemove',function(val){
-    // console.log('mousemove event performed');
-    // console.log(val); //gives event details
-})
-// mouseout
-main.addEventListener('mouseout',function(val){
-    // console.log('mouseout event performed');
-    // console.log(val); //gives event details
-})
-//Difference between mouseover and mouseenter events
-// mouseover fires when entering an element or its children and bubbles, 
-// while mouseenter fires only once when entering the element itself and does not bubble.
-
+        const note = key.dataset.note;
+        const audio = new Audio(`sounds/${note}.mp3`);
+        audio.play();
+        key.classList.add('active');
+        setTimeout(() => key.classList.remove('active'),200);
+    
+});
 
 
 
