e9822b580473e5fcc0e8833cd78eb7ae1f86d05e
Navyachhokar <chhokarnavya@gmail.com>
2026-02-08 08:58:11 +0530

    basic console questions practice

---
+++ js_dsa/basics/console/script.js
@@ -0,0 +1,40 @@
+let fullName = "Navya";
+let hobby = "Dancing";
+
+// console.log(`My name is ${fullName} and my hobbby is ${hobby}`);
+
+let calc = 45*2-10;
+// console.log(calc);
+
+let currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+let firstName = "Navya";
+let lastName = "Chhokar";
+// console.log(`${firstName} ${lastName}`);
+
+var temp = 0;
+// console.log(temp);
+temp += 2;
+// console.log(temp);
+
+// console.error("error occured")
+
+let num = 5;
+// console.log(num*num);
+// console.log(num**2);Preferrable method is exponentiation method.
+// console.log(Math.pow(num,2));
+
+const bool = true;
+// console.log(bool);
+
+let age = 21;
+(age>18)?console.log("greater than 18"):console.log("not greater than 18");
+// ;
+
+let number = 100/0;
+// console.log(number);
+
+
+
+
