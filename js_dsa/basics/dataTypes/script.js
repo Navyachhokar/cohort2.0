33a1bb8f5e5fbb667a4b0d5191a050d71aa8e0ad
Navyachhokar <chhokarnavya@gmail.com>
2026-02-08 09:16:04 +0530

    Basic data types practice questions

---
+++ js_dsa/basics/dataTypes/script.js
@@ -0,0 +1,34 @@
+let variable = 5;
+// console.log(variable);
+
+const pi = 3.14;
+// console.log(pi);
+
+let num = 6;
+// console.log(num);
+num = 9;
+// console.log(num);
+
+let temp = null;
+// console.log(typeof temp);
+
+let str = "6";
+// console.log(typeof str);
+
+let bool = false;
+// console.log(typeof bool);
+
+// console.log(`Number:${num} \nString:${str} \nBool:${bool}`);
+
+let x ;
+// console.log(x);
+
+let y = undefined;
+// console.log(typeof y);
+
+const arr = [2,4,5,6];
+arr[2] = 9;
+// console.log(arr);
+
+const arr2 = [...arr,4];
+// console.log(arr2);
