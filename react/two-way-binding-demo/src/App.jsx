c81386fd81b748f7d0b845fb2bcec4615e764962
Navyachhokar <chhokarnavya@gmail.com>
2026-02-21 20:47:32 +0530

    feat: implement two-way binding for contact form

--- react/two-way-binding-demo/src/App.jsx
+++ react/two-way-binding-demo/src/App.jsx
@@ -1,35 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[title, setTitle] = useState('');
+  const[email, setEmail] = useState('');
+  const[allUsers, setAllUsers] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setAllUsers([...allUsers, {title,email}])
+    setTitle('');
+    setEmail('');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=" flex flex-col justify-center bg-amber-200 h-screen p-8 ">
+        <form
+          className="flex flex-col h-[80%] w-[50%] bg-white justify-evenly rounded-2xl p-3 self-center"
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <input
+            className="p-4 m-4 rounded-2xl border bg-amber-50"
+            type="text"
+            placeholder="Enter name"
+            value={title}
+            required
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <input
+            className="p-4 m-4 rounded-2xl border bg-amber-50"
+            type="text"
+            placeholder="Enter email"
+            value={email}
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <button className="flex flex-col bg-amber-300 border rounded-xl self-center px-2 font-light mx-10 ">
+            Submit
+          </button>
+        </form>
+        <h2 className=" text-4xl font-bold text-white text-center mt-3">
+          Users List
+        </h2>
+        {allUsers.map(function (elem, idx) {
+          return (
+            <div
+              className="flex flex-col h-[15%] w-[50%] justify-center self-center "
+              key={idx}
+            >
+              <div className="bg-white px-4 rounded-2xl text-s font-light ">
+                <h4 className="">{elem.title}</h4>
+                <p className="">{elem.email}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
