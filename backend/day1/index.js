48ccfdb2b94afa5faf35df4e7eb36aea071ca84b
Navyachhokar <chhokarnavya@gmail.com>
2026-03-08 22:17:56 +0530

    day 1 backend

---
+++ backend/day1/index.js
@@ -0,0 +1,6 @@
+// console.log("hello");
+//use `node {filename}` to run this file on the terminal outside of the browser.
+
+const catMe = require("cat-me"); // require package
+
+console.log(catMe()); //call it and log it
